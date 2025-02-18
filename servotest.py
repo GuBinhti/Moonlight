c9155111f9cfc9e3462b6416224546d11bd54516
GuBinhti <92348224+GuBinhti@users.noreply.github.com>
2025-02-18 14:52:28 -0800

    Update servotest.py

--- servotest.py
+++ servotest.py
@@ -1,4 +1,4 @@
-from skyfield.api import load, wgs84
+from skyfield.api import load,Topos
 from datetime import datetime, timedelta
 import RPi.GPIO as GPIO
 import time
@@ -14,7 +14,7 @@ def setup_servo(pin):
     return pwm
 
 def set_servo_angle(pwm, angle):
-    duty_cycle = ((angle / 180) * 10) + 2.5  # Maps 0°-180° to 2.5%-12.5% duty cycle
+    duty_cycle = ((500 + (1500*angle)) / 20000) * 100  # Maps 0°-180° to 2.5%-12.5% duty cycle
     pwm.ChangeDutyCycle(duty_cycle)
     time.sleep(0.5)
     pwm.ChangeDutyCycle(0)
@@ -28,7 +28,7 @@ def moon_placement_azimuth(year, month, day, hour, minute, second, pwm):
 
     latitude = 0
     longitude = -119.8610
-    location = wgs84.latlon(latitude, longitude)
+    location = Topos(latitude_degrees=latitude, longitude_degrees=longitude)
 
     moon = planets['moon']
     earth = planets['earth']
@@ -74,3 +74,20 @@ if __name__ == '__main__':
     finally:
         servo_pwm.stop()
         GPIO.cleanup()
+
+Traceback (most recent call last):
+  File "/home/moonlight/Desktop/ARM/servo.py", line 72, in <module>
+    moon_placement_azimuth(year, month, day, hour, minute, second, servo_pwm)
+  File "/home/moonlight/Desktop/ARM/servo.py", line 54, in moon_placement_azimuth
+    set_servo_angle(pwm, servo_angle)
+  File "/home/moonlight/Desktop/ARM/servo.py", line 18, in set_servo_angle
+    pwm.ChangeDutyCycle(duty_cycle)
+  File "/usr/lib/python3/dist-packages/RPi/GPIO/__init__.py", line 215, in ChangeDutyCycle
+    raise ValueError('dutycycle must have a value from 0.0 to 100.0')
+ValueError: dutycycle must have a value from 0.0 to 100.0
+Exception ignored in: <function PWM.__del__ at 0x7fff32f50c20>
+Traceback (most recent call last):
+  File "/usr/lib/python3/dist-packages/RPi/GPIO/__init__.py", line 179, in __del__
+  File "/usr/lib/python3/dist-packages/RPi/GPIO/__init__.py", line 202, in stop
+  File "/usr/lib/python3/dist-packages/lgpio.py", line 1084, in tx_pwm
+TypeError: unsupported operand type(s) for &: 'NoneType' and 'int'
